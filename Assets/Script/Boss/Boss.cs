b0101bba3f7b3c3c16f9a949f419a09a1498cd04
HB0814 <147120757+HB0814@users.noreply.github.com>
2023-11-21 10:55:00 +0900

    2023-11-21-10-54 백업

---
+++ Assets/Script/Boss/Boss.cs
@@ -0,0 +1,494 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Enemy
+{
+    public enum Boss_Type { BossA, BossB, BossC }
+    public Boss_Type bossType;
+
+    public int patternIndex;
+    public int deathHitCount;
+    public int attackDamage;
+
+    float rageGage;
+
+    float frontPos;
+
+    //BossA
+    public float rollingSpeed;
+    public float PatternDelay;
+    public float curShotDelay;
+    public float maxShotDelay;
+
+    float normalScatchRatio;
+
+    float rageScatchRatio;
+    float rageRollingRatio;
+
+    float doReadyRoll;
+
+    bool isRolling;
+
+    bool isRolling_1;
+    bool isRolling_2;
+
+    int scaleCount;
+    bool isScalesFire;
+
+    public bool isAppear;
+    public bool rageState;
+
+    public CircleCollider2D Rolling;
+
+
+    public Enemy enemyScript;
+
+    void Awake()
+    {
+        normalScatchRatio = 7;
+        rageScatchRatio = 4;
+        rageRollingRatio = 7;
+        doReadyRoll = 5;
+
+        rollingSpeed = 25;
+
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    void OnEnable()
+    {
+        isAppear = true;
+        StartCoroutine(LevelCheck());
+    }
+    void FixedUpdate()
+    {
+        Player playerLogic = player.GetComponent<Player>();
+        if (playerLogic.isBoss&&isAppear)
+            Appear();
+
+        if (!isDie)
+        {
+            dist = Vector2.Distance(player.transform.position, transform.position);
+
+            Follow();
+            //BossA
+            {
+                //Moving
+                if (!isAttack && !isAppear)
+                {
+                    Move();
+                    anim.SetBool("isWalk", true);
+                }
+                //ScaleShot
+                else if (isScalesFire)
+                {
+                    curShotDelay += Time.deltaTime;
+                    ScalesAttack();
+                }
+                if (rageState)
+                    RageSprite();
+
+            }
+        }
+        else
+        {
+            DieSprite();
+        }
+
+
+    }
+
+    //Start Setting
+    public void Appear()
+    {
+        isAppear = false;
+        StartCoroutine(Think());
+    }
+    IEnumerator LevelCheck()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Enemy enemyLogic = enemyScript.GetComponent<Enemy>();
+        level = enemyLogic.level;
+
+        switch (bossType)
+        {
+            case Boss_Type.BossA:
+
+                break;
+            case Boss_Type.BossB:
+
+                break;
+            case Boss_Type.BossC:
+
+                break;
+        }
+        switch (level)
+        {
+            case Level.Easy:
+                health = 100;
+                maxHealth = 100;
+                dmg = 15;
+                rageGage = 30;
+                scaleCount = 5;
+                break;
+
+            case Level.Normal:
+
+                break;
+
+            case Level.Hard:
+                health = 100;
+                maxHealth = 100;
+                dmg = 20;
+                rageGage = 30;
+                scaleCount = 9;
+                break;
+
+        }
+    }
+
+    //Move
+    void Move()
+    {
+        rigid.velocity = new Vector2(frontPos * speed, rigid.velocity.y);
+    }
+    void Follow()
+    {
+        if (player.transform.position.x - transform.position.x < 0)
+        {
+            frontPos = -speed;
+            spriteRenderer.flipX = false;
+        }
+        else if (player.transform.position.x - transform.position.x >= 0)
+        {
+            frontPos = speed;
+            spriteRenderer.flipX = true;
+        }
+    }
+    void RandomPattern()
+    {
+        int ranPattern = Random.Range(0, 10);
+        if (!rageState)
+        {
+            if (ranPattern < normalScatchRatio) //70%
+                patternIndex = 0;
+            else                                //30%
+                patternIndex = 1;
+        }
+        else
+        {
+            if (ranPattern < rageScatchRatio)         //40%
+                patternIndex = 0;
+            else if(ranPattern< rageRollingRatio)       //30%
+                patternIndex = 1;
+            else                        //30%
+                patternIndex = 2;
+        }
+    }
+    IEnumerator Think()
+    {
+        if(isDie)
+            yield break;
+
+        isAppear = false;
+        isAttack = false;
+
+        rigid.velocity = Vector2.zero;
+
+        if (!rageState)
+            ReturnSprite(1f);
+        //Rage Mode
+        if (health <= maxHealth * (rageGage / 100f) && !rageState)
+        {
+            isAttack = true;
+            rageState = true;
+            yield return new WaitForSeconds(3);
+
+            Rage();
+            StartCoroutine(Think());
+            yield break;
+        }
+        RandomPattern();
+        switch(patternIndex)
+        {
+            case 0:     //Scratch&ReThink
+                if(dist<5)
+                {
+                    yield return new WaitForSeconds(PatternDelay);
+                    StartCoroutine(ScratchAttack());
+                }
+                else
+                {
+                    yield return new WaitForSeconds(PatternDelay);
+                    StartCoroutine(Think());
+                }
+                break;
+            case 1:     //Roll
+                StartCoroutine(RollAttack());
+                break;
+            case 2:     //Jump
+                StartCoroutine(JumpAttack());
+                break;
+            default:
+                StartCoroutine(Think());
+                break;
+        }
+    }
+
+    //Rolling Attack
+    IEnumerator RollAttack()
+    {
+        float rollDmg = dmg;
+        yield return null;
+
+        dmg = rollDmg * 1.2f;
+        isRolling = true;
+        isAttack = true;
+        anim.SetTrigger("StartRoll");
+        rigid.AddForce(Vector2.right * frontPos * -doReadyRoll + Vector2.up * doReadyRoll, ForceMode2D.Impulse);
+
+        int ranRoll = Random.Range(0, 2);
+        yield return new WaitForSeconds(PatternDelay);
+
+        Rolling.enabled = true;
+        if (ranRoll == 0)
+            isRolling_1 = true;
+
+        if (ranRoll == 1)
+            isRolling_2 = true;
+
+        rigid.AddForce(Vector2.right * frontPos * rollingSpeed, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(PatternDelay);
+
+        anim.SetTrigger("EndRoll");
+        dmg = rollDmg;
+        Stun();
+        Rolling.enabled = false;
+        yield return new WaitForSeconds(PatternDelay * 2);
+
+        StartCoroutine(Think());
+    }
+
+    //Scratch Attack
+    IEnumerator ScratchAttack()
+    {
+        isAttack = true;
+        anim.SetBool("isWalk", false);
+        yield return null;
+
+        GameObject scratch = objectManager.MakeObj("bossAAttack_0");
+        BossAttackObject scratchLogic = scratch.GetComponent<BossAttackObject>();
+        scratchLogic.enemyScript = enemyScript;
+        scratchLogic.dmg = dmg;
+        scratchLogic.Att_type = BossAttackObject.Attack_Type.Melee;
+
+        scratch.transform.position = transform.position + Vector3.right * frontPos*2+Vector3.down*0.2f;
+
+        yield return new WaitForSeconds(PatternDelay);
+        StartCoroutine(Think());
+    }
+
+    //Jump Attack
+    IEnumerator JumpAttack()
+    {
+        isAttack = true;
+        yield return new WaitForSeconds(1f);
+
+        anim.SetTrigger("doJump");
+        rigid.AddForce(Vector2.up * 30, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1f);
+
+
+        anim.SetTrigger("doLand");
+        gameManager.DropDebris();
+
+        StartCoroutine(Think());
+    }
+
+    //Scales Attack
+    void ScalesAttack()
+    {
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        int ran = Random.Range(0, 10);
+
+        if (ran < 3)   //30%
+        {
+            for (float index = 0; index < scaleCount; index++)
+            {
+                GameObject scales = objectManager.MakeObj("bossAAttack_1");
+                BossAttackObject scaleLogic = scales.GetComponent<BossAttackObject>();
+                scaleLogic.enemyScript = enemyScript;
+                scaleLogic.dmg = dmg * 0.7f;
+                scaleLogic.Att_type = BossAttackObject.Attack_Type.Range;
+
+                scales.transform.position = transform.position+Vector3.down*1f;
+                Rigidbody2D S_rigid = scales.GetComponent<Rigidbody2D>();
+                Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * index / (scaleCount-1)),
+                    Mathf.Sin(Mathf.PI * index / (scaleCount - 1)));
+
+                S_rigid.transform.Rotate(Vector3.forward * index * 45);
+                S_rigid.AddForce(dirVec * 5, ForceMode2D.Impulse);
+            }
+        }
+        curShotDelay = 0;
+    }
+
+    //Speed*1.25
+    void Rage()
+    {
+        isScalesFire = true;
+        isAttack = false;
+        rageState = true;
+        PatternDelay = 1.2f;
+        speed *= 1.25f ;
+    }
+
+    //Rolling Stun
+    void Stun()
+    {
+        isRolling = false;
+
+        isRolling_1 = false;
+        isRolling_2 = false;
+
+        rigid.velocity = Vector2.zero;
+        transform.rotation = Quaternion.identity;
+
+        ReturnSprite(0.7f);
+    }
+
+    //Hit
+    IEnumerator OnHit(float dmg)
+    {
+        if (isHit)
+            yield break;
+
+        if (health > 0)
+        {
+            isHit = true;
+            DamageLogic(dmg);
+
+            ReturnSprite(0.4f);
+
+            //Rolling Animation
+            if (!isRolling)
+            {
+                anim.SetTrigger("doHit");
+            }
+            yield return new WaitForSeconds(0.2f);
+            isHit = false;
+            ReturnSprite(1f);
+        }
+        if (health <= 0)
+        {
+            //Dead Animation
+            if (!isDie)
+            {
+                anim.SetTrigger("doDie");
+                isDie = true;
+                yield return new WaitForSeconds(2.2f);
+            }
+
+            ReturnSprite(0.3f);
+            deathHitCount++;
+            
+            yield return new WaitForSeconds(0.2f);
+
+            isHit = false;
+            //Item
+            for (int index = 0; index < 5; index++)
+            {
+                Vector2 ranVec = new Vector2(Random.Range(-1f, 1f), 0);
+                if (deathHitCount < 2)
+                {
+                    GameObject b_Coin = objectManager.MakeObj("bronze");
+                    b_Coin.transform.position = transform.position;
+
+                    Rigidbody2D b_Rigid = b_Coin.GetComponent<Rigidbody2D>();
+                    b_Rigid.AddForce(ranVec * 2 + Vector2.up * 8, ForceMode2D.Impulse);
+                }
+                else if (deathHitCount < 3)
+                {
+                    GameObject s_Coin = objectManager.MakeObj("silver");
+                    s_Coin.transform.position = transform.position;
+
+                    Rigidbody2D s_Rigid = s_Coin.GetComponent<Rigidbody2D>();
+                    s_Rigid.AddForce(ranVec * 2 + Vector2.up * 8, ForceMode2D.Impulse);
+                }
+                else if (deathHitCount < 4)
+                {
+                    GameObject g_Coin = objectManager.MakeObj("gold");
+                    g_Coin.transform.position = transform.position;
+
+                    Rigidbody2D g_Rigid = g_Coin.GetComponent<Rigidbody2D>();
+                    g_Rigid.AddForce(ranVec * 2 + Vector2.up * 8, ForceMode2D.Impulse);
+                }
+                else if(deathHitCount==4)
+                {
+                    GameObject coreA = objectManager.MakeObj("coreA");
+                    coreA.transform.position = transform.position;
+
+                    Rigidbody2D rigid = coreA.GetComponent<Rigidbody2D>();
+                    rigid.AddForce(Vector2.up * 8, ForceMode2D.Impulse);
+                    yield return new WaitForSeconds(2.2f);
+
+                    gameObject.SetActive(false);
+                    yield break;
+                }
+                else
+                    yield break;
+            }
+        }
+    }
+    void ReturnSprite(float Alpha)
+    {
+        spriteRenderer.color = new Color(0.4f, 0.4f, 0.4f, Alpha);
+    }
+
+    //Sprite
+    void RageSprite()
+    {
+        spriteRenderer.color = new Color(1, 0.5f, 0.5f, 1);
+    }
+    void DieSprite()
+    {
+        ReturnSprite(0.5f);
+    }
+
+    //Tigger
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDie)
+            return;
+        if(collision.gameObject.tag=="Border")
+        {
+            if (isRolling_1)
+            {
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                gameManager.SpawnMouse();
+            }
+            if (isRolling_2)
+            {
+                rigid.velocity = Vector2.zero;
+                rigid.AddForce(Vector2.right * frontPos * rollingSpeed, ForceMode2D.Impulse);
+                gameManager.SpawnMouse();
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //플레이어의 무기에 공격당했을 때 
+        if (collision.gameObject.tag == "PlayerAttack")
+        {
+            Player playerLogic = player.GetComponent<Player>();
+            StartCoroutine(OnHit(playerLogic.dmg));
+
+        }
+    }
+}
---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,505 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //소환수
+    public int followCount;
+    int maxFollowCount;
+    int hpDecrease;
+
+    //공격 타입
+    public enum Att_Type { Normal,Power,Sharp,Mystic}
+    public Att_Type type;
+
+    //특수 코어
+    public enum Skill_Core {Roll, Summon,DropAttack,Crouch }
+    public Skill_Core skill_Type;
+
+    //플레이어 스테이터스
+    public float curLife;
+    public float maxLife;
+    public float dmg;
+    public float jumpPower;
+    public float speed;
+    float roll_Speed;
+
+    public float coin;
+
+    //공격 딜레이
+    float curAttackDelay;
+    float maxAttackDelay;
+
+    public bool clearMap;       //클리어 시 트리거 작동(미구현)
+    public bool isStart;        //시작 전 컨트롤제어
+
+    //스테이터스 코어(미구현)
+    public bool damageCore;
+    public bool speedCore;
+    public bool healthCore;
+
+    //공격타입 트리거
+    public bool isNormal;
+    public bool isPower;
+    public bool isSharp;
+    public bool isMystic;
+
+    bool Q_IsSwitch;
+    bool E_IsSwitch;
+
+    //Controller
+    float Move_Axis;
+
+    bool isGround;
+    bool isJump;
+    bool jumping;
+
+    bool isAtt;
+    bool isCrouch;
+
+    public bool startSkill;
+    public bool endSkill;
+
+    //Trigger
+    bool isTouchRoom;       //투명벽 구현
+    public bool isBoss;     //보스전 시작
+
+    bool isHit;
+
+    bool isCheat;
+
+
+    public ObjectManager objectManager;
+    public GameManager gameManager;
+    public BoxCollider2D meleeAttack;
+    public CircleCollider2D rollAttack;
+
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+    PlatformEffector2D effector;
+
+    void Awake()
+    {
+        maxLife = 100;
+        curLife = 100;
+        dmg = 15;
+        speed = 5;
+        roll_Speed = speed;
+
+        maxAttackDelay = 0.5f;
+
+        maxFollowCount = 3;
+        followCount = 0;
+        hpDecrease = 25;
+
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        effector = GetComponent<PlatformEffector2D>();
+
+        isNormal = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            gameManager.ShakeCam();
+        InPut();
+        Input_Skill();
+
+        MoveMent();
+        Jump();
+        LifeCheck();
+        StartCoroutine(Attack());
+        StartCoroutine(Skill());
+
+        StartCoroutine(Attack_Type());
+
+        StartCoroutine(LifeCheat());
+    }
+    //플레이어 이동
+    void MoveMent()
+    {
+        if (isTouchRoom && !clearMap&& Move_Axis == 1)
+        {
+            Move_Axis = 0;
+        }
+        if (isBoss|| isCrouch)
+            Move_Axis = 0;
+
+        rigid.velocity = new Vector2(Move_Axis * speed, rigid.velocity.y);  
+        if (Move_Axis != 0)
+        {
+            if (Move_Axis > 0)
+            {
+                spriteRenderer.flipX = false;
+                meleeAttack.offset = new Vector2(0.6f, 0);
+            }
+            else if (Move_Axis < 0)
+            {
+                spriteRenderer.flipX = true;
+                meleeAttack.offset = new Vector2(-0.6f, 0);
+            }
+        }
+    }
+    //시작 전 동작 불가
+    void InPut()
+    {
+        if (!isStart||isCrouch)
+            return;
+
+        Move_Axis = Input.GetAxisRaw("Horizontal"); //이동
+        isAtt=Input.GetKeyDown(KeyCode.A);          //공격
+        isJump=Input.GetKeyDown(KeyCode.S);         //점프
+        Q_IsSwitch = Input.GetKeyDown(KeyCode.Q);   //공격 타입 슬롯체인지 노멀->파워->정밀->신비
+        E_IsSwitch = Input.GetKeyDown(KeyCode.E);   //공격 타입 슬롯체인지 노멀->신비->정밀->파워
+        isCheat = Input.GetKeyDown(KeyCode.L);      //치트키= 체력 100증가
+
+    }
+    void Input_Skill()
+    {
+        startSkill = Input.GetKeyDown(KeyCode.D);      //스킬
+        endSkill = Input.GetKeyUp(KeyCode.D);
+    }
+    void Jump()
+    {
+        if (rigid.velocity.y == 0)
+        {
+            isGround = true;
+            jumping = false;
+        }
+        if (isJump && jumping)
+        {
+            anim.SetTrigger("doJump");
+            rigid.velocity = new Vector2(rigid.velocity.x, 0f);
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumping = false;
+        }
+        if (isJump&&isGround)
+        {
+            anim.SetTrigger("doJump");
+            rigid.velocity = new Vector2(rigid.velocity.x, 0f);
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGround = false;
+            jumping = true;
+        }
+    }
+    IEnumerator Attack()
+    {
+        curAttackDelay += Time.deltaTime;
+        if (curAttackDelay < maxAttackDelay)
+            yield break;
+        if(isBoss || !isAtt)
+            yield break;
+
+        meleeAttack.enabled = true;
+        anim.SetTrigger("doAttack");
+        yield return new WaitForSeconds(0.1f);
+
+        meleeAttack.enabled = false;
+        isAtt = false;
+        curAttackDelay = 0;
+    }
+
+    //스킬
+    IEnumerator Skill()
+    {
+        if (startSkill|| endSkill)
+        {
+            switch (skill_Type)
+            {
+                case Skill_Core.Roll:
+                    if (!endSkill)
+                    {
+                        isHit = true;
+                        Physics2D.IgnoreLayerCollision(6, 7,true);
+                        rollAttack.enabled = true;
+
+                        for (int index = 0; index < 10;)
+                        {
+                            yield return new WaitForSeconds(0.15f);
+                            speed = speed+(roll_Speed * 0.1f);
+                            index++;
+                        }
+
+                        endSkill = false;
+                        startSkill = false;
+                    }
+                    else
+                    {
+                        isHit = false;
+                        Physics2D.IgnoreLayerCollision(6, 7, false);
+                        rollAttack.enabled = false;
+
+                        for (int index = 0; index < 10;)
+                        {
+                            yield return new WaitForSeconds(0.5f);
+                            speed = speed - (roll_Speed * 0.1f);
+                            index++;
+                        }
+                        yield return null;
+                        endSkill = false;
+                        startSkill = false;
+                    }
+
+                    break;
+
+                case Skill_Core.Summon:
+                    
+                    if (curLife <= hpDecrease || followCount > maxFollowCount||endSkill)
+                        yield break;
+
+                    followCount++;
+                    gameManager.FollowerSpawn();
+                    maxLife -= hpDecrease;
+                    startSkill = false;
+                    yield return null;
+                    break;
+
+                case Skill_Core.DropAttack:
+
+                    break;
+                case Skill_Core.Crouch:
+                    if (!endSkill)
+                    {
+                        isHit = true;
+                        isCrouch = true;
+                        anim.SetBool("isLieDown", true);
+                        Physics2D.IgnoreLayerCollision(6, 7, true);
+
+                        endSkill = false;
+                        startSkill = false;
+                    }
+                    else
+                    {
+                        isHit = false;
+                        isCrouch = false;
+                        anim.SetBool("isLieDown", false);
+                        Physics2D.IgnoreLayerCollision(6, 7, false);
+
+                        yield return null;
+                        endSkill = false;
+                        startSkill = false;
+                    }
+                 break;
+            }
+        }
+
+        yield return null;
+    }
+
+    //체력 확인
+    void LifeCheck()
+    {
+        if (curLife > maxLife)
+            curLife = maxLife;
+    }
+
+    //공격타입 로직
+    IEnumerator Attack_Type()
+    {
+        if (Q_IsSwitch)
+        {
+            switch (type)
+            {
+                case Att_Type.Normal:
+                    isNormal = false;
+                    isPower = true;
+                    type = Att_Type.Power;
+                    break;
+                case Att_Type.Power:
+                    isPower = false;
+                    isSharp = true;
+                    type = Att_Type.Sharp;
+                    break;
+                case Att_Type.Sharp:
+                    isSharp = false;
+                    isMystic = true;
+                    type = Att_Type.Mystic;
+                    break;
+                case Att_Type.Mystic:
+                    isMystic = false;
+                    isNormal = true;
+                    type = Att_Type.Normal;
+                    break;
+            }
+            yield return new WaitForSeconds(0.5f);
+
+            Q_IsSwitch = false;
+        }
+        if(E_IsSwitch)
+        {
+            switch (type)
+            {
+                case Att_Type.Normal:
+                    isNormal = false;
+                    isMystic = true;
+                    type = Att_Type.Mystic;
+                    break;
+                case Att_Type.Power:
+                    isPower = false;
+                    isNormal = true;
+                    type = Att_Type.Normal;
+                    break;
+                case Att_Type.Sharp:
+                    isSharp = false;
+                    isPower = true;
+                    type = Att_Type.Power;
+                    break;
+                case Att_Type.Mystic:
+                    isMystic = false;
+                    isSharp = true;
+                    type = Att_Type.Sharp;
+                    break;
+            }
+            yield return new WaitForSeconds(0.5f);
+
+            E_IsSwitch = false;
+        }
+        yield return null;
+    }
+
+    //치트     Life+100
+    IEnumerator LifeCheat()
+    {
+        if (!isCheat)
+            yield break;
+
+        yield return null;
+        curLife += 100;
+        maxLife += 100;
+
+        yield return new WaitForSeconds(0.5f);
+        isCheat = false;
+    }
+
+    //피격
+    IEnumerator OnHit(float dmg)
+    {
+        if (isHit)
+            yield break;
+
+        isHit = true;
+        curLife -= dmg;
+        ReturnSprite(0.8f);
+        anim.SetTrigger("doHit");
+        rigid.velocity = Vector2.zero;
+        rigid.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
+
+        if (curLife < 0)
+        {
+            gameObject.SetActive(false);
+        }
+        yield return new WaitForSeconds(0.3f);
+
+        isHit = false;
+        ReturnSprite(1f);
+    }
+    void ReturnSprite(float Alpha)
+    {
+        spriteRenderer.color = new Color(0.4f, 0.4f, 0.4f, Alpha);
+    }
+    //보스 시작 트리거
+    IEnumerator BossStart()
+    {
+        isBoss = true;
+        yield return new WaitForSeconds(0.1f);
+
+        isBoss = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Enemy")
+        {
+            Enemy enemyLogic = collision.gameObject.GetComponentInParent<Enemy>();
+
+            StartCoroutine(OnHit(enemyLogic.dmg));
+        }
+
+        //Item
+        if (collision.gameObject.tag == "Item")
+        {
+            Item itemLogic = collision.gameObject.GetComponent<Item>();
+            switch (itemLogic.itemType)
+            {
+                case Item.ItemType.Coin:
+                    coin += itemLogic.coin;
+                    break;
+
+                case Item.ItemType.Core:
+                    switch (itemLogic.coreType)
+                    {
+                        case Item.CoreType.Damage:
+                            damageCore = true;
+                            break;
+                        case Item.CoreType.Speed:
+                            speedCore = true;
+                            break;
+                        case Item.CoreType.Health:
+                            healthCore = true;
+                            break;
+                    }
+                    break;
+            }
+            collision.gameObject.SetActive(false);
+        }
+        //Ground
+        if(collision.gameObject.tag=="Ground")
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyAttack")
+        {
+            EnemyObject enemyLogic = collision.gameObject.GetComponent<EnemyObject>();
+
+            StartCoroutine(OnHit(enemyLogic.dmg));
+        }
+        if (collision.gameObject.tag == "BossAttack")
+        {
+            BossAttackObject objectLogic = collision.gameObject.GetComponent<BossAttackObject>();
+            switch (objectLogic.Att_type)
+            {
+                case BossAttackObject.Attack_Type.Melee:
+                    StartCoroutine(OnHit(objectLogic.dmg));
+                    break;
+                case BossAttackObject.Attack_Type.Range:
+                    StartCoroutine(OnHit(objectLogic.dmg));
+                    break;
+            }
+        }
+        if (collision.gameObject.tag == "Boss")
+        {
+            Boss bossLogic =collision.gameObject.GetComponentInParent<Boss>();
+
+            StartCoroutine(OnHit(bossLogic.dmg));
+        }
+        if(collision.gameObject.tag=="TriggerMap")
+        {
+            isTouchRoom = true;
+        }
+        if(collision.gameObject.tag=="BossTrigger")
+        {
+            StartCoroutine(BossStart());
+            collision.gameObject.SetActive(false);
+        }
+        if(collision.gameObject.tag=="Debris")
+        {
+            Debris debrisLogic = collision.gameObject.GetComponent<Debris>();
+            collision.gameObject.SetActive(false);
+
+            StartCoroutine(OnHit(debrisLogic.dmg));
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "TriggerMap")
+        {
+            isTouchRoom = false;
+        }
+    }
+}
