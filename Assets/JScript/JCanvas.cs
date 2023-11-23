491f72db06287ee1722c1e49de75855352ef6861
JANGSAEHYEON <rokuel6l@metaskyworld.net>
2023-11-24 01:47:42 +0900

    2023-11-24 01:47 UI

--- Assets/JScript/JCanvas.cs
+++ Assets/JScript/JCanvas.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
 
@@ -25,8 +26,9 @@ public class JCanvas : MonoBehaviour
     /// <summary> [게임 오브젝트] 엔드 버튼 오브젝트 </summary>
     [Header("메인화면 끝 버튼")]
     public GameObject EndBtn;
-    
-    [Header("==============================================")][Space(1f)]
+
+    [Header("==============================================")] [Space(1f)]
+    private int a;
     
     /// <summary> [게임 오브젝트] 인 게임 캔버스 </summary>
     [Header("인 게임 캔버스")]
@@ -40,7 +42,17 @@ public class JCanvas : MonoBehaviour
     /// <summary> [게임 오브젝트] 엔드 버튼 오브젝트 </summary>
     [Header("인 게임 끝 버튼")]
     public GameObject InGameEndBtn;
+
+    [Header("==============================================")] [Space(1f)]
+    private bool isOpen;
     
+    /// <summary> [게임 오브젝트] 엔드 버튼 오브젝트 </summary>
+    [Header("창 인벤토리")]
+    public GameObject InventoryWindow;
+    
+    /// <summary> [게임 오브젝트] 엔드 버튼 오브젝트 </summary>
+    [Header("창 세팅")]
+    public GameObject SettingWindow;
 
     // Start is called before the first frame update
     void Start()
@@ -51,8 +63,34 @@ public class JCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isOpen)
+            {
+                if(!InGameCanvas.activeSelf)
+                    InGameCanvas.SetActive(true);
+                InGameCanvas.GetComponent<CanvasGroup>().DOFade(1f, 0.2f);
+                isOpen = true;
+            }
+            else
+            {
+                InGameCanvas.GetComponent<CanvasGroup>().DOFade(0f, 0.2f);
+                InGameCanvas.SetActive(false);
+                isOpen = false;
+            }
+        }
     } 
+    
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//               < 사용자 지정 함수 >
+//==================================================================================
+//==================================================================================
+//==================================================================================
+    
+    
+    
     
 //==================================================================================
 //==================================================================================
@@ -74,15 +112,31 @@ public class JCanvas : MonoBehaviour
     {
         MainCanvas.SetActive(false);
     }
-
+    /// <summary> 버튼 올리면 켜지기 </summary>
     public void ESCButtonEnterEvent(GameObject Btn)
     {
         Btn.GetComponent<Image>().DOFade(0.2f, 0.3f);
     }
-    
+    /// <summary> 버튼 벗어나면 끄기 </summary>
     public void ESCButtonExitEvent(GameObject Btn)
     {
         Btn.GetComponent<Image>().DOFade(0f, 0.3f);
     }
+
+    public void WindowOpenEvent(GameObject Canvas)
+    {
+        InventoryWindow.GetComponent<CanvasGroup>().DOFade(0f, 0.2f);
+        SettingWindow.GetComponent<CanvasGroup>().DOFade(0f, 0.2f);
+        Canvas.SetActive(true);
+        Canvas.GetComponent<CanvasGroup>().DOFade(1f, 0.3f);
+    }
+    
+    public void WindowCloseEvent(GameObject Canvas)
+    {
+        Canvas.GetComponent<CanvasGroup>().DOFade(0f, 0.3f);
+        Canvas.SetActive(false);
+    }
+    
+    
     
 }
