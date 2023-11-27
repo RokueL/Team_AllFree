f83d181d9d3525223b0ad2c911b8c16a548fa290
RokueL <kazix135@gmail.com>
2023-11-27 22:37:17 +0900

    Delete Assets directory

--- Assets/HB/Script/Manager/CoreInf.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CoreInf : MonoBehaviour
-{
-    public int increaseDamage;
-    public int increaseHealth;
-}
